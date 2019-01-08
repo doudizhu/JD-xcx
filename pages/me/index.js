4843e34d57365ab62d70d99c97601975aa718d73
lipeng <lipeng@daokoudai.com>
2019-01-08 17:22:11 +0800

    7.微信小程序-解决用户授权

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,3 +1,7 @@
+// 获取应用实例
+const app = getApp();
+// console.log(app);
+
 // pages/me/index.js
 Page({
 
@@ -14,7 +18,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo:app.globalData.userInfo,
+        hasUserInfo:true
+      })
+    }else if(this.data.canIUse){
+      // 由于getUserInfo是网络请求，可能会在Page.onload之后才返回
+      // 为了防止这种情况发生，所以此处加入callback
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo:res.userInfo,
+          hasUserInfo:true
+        })
+      }
+    }
   },
 
   // 实现获取用户信息的方法
