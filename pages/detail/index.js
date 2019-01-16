c8f92baf7a017f6e1c318c86c57aec3d871291a8
lipeng <lipeng@daokoudai.com>
2019-01-16 13:57:32 +0800

    25.微信小程序-加入购物车本地存储

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -127,6 +127,59 @@ Page({
     })
   },
   addCart(){
-    console.log('加入购物车')
+    // console.log('加入购物车')
+    let self = this
+
+    wx.getStorage({
+      key: 'cartInfo',
+      // 查到cartInfo这个数据了，判断数组中是否拥有当前添加的商品
+      success: function(res) {
+        // console.log(res)
+        const cartArray = res.data
+        // 拿到现在添加的商品对象
+        const partData = self.data.partData
+
+        let isExit = false // 判断数组是否存在该商品
+
+        // 匹配本地数组和当前对象的id是否一致
+        // 如果商品存在，仅更新对应total值
+        cartArray.forEach(cart =>{
+          if(cart.id == partData.id){
+            isExit = true
+            cart.total += self.data.partData.count
+            wx.setStorage({
+              key: 'cartInfo',
+              data: cartArray,
+            })
+          }
+        })
+
+        // 不存在商品
+        if(!isExit){
+          partData.total = self.data.partData.count
+          cartArray.push(partData)
+          wx.setStorage({
+            key: 'cartInfo',
+            data: cartArray,
+          })
+        }
+      },
+      fail(res){
+        let partData = self.data.partData
+        partData.total = self.data.partData.count
+        // console.log(partData)
+        let cartArray = []
+        cartArray.push(partData)
+        wx.setStorage({
+          key: 'cartInfo',
+          data: cartArray,
+        })
+      }
+    })
+    wx.showToast({
+      title: '加入购物车成功',
+      icon: 'success',
+      duration: 3000,
+    })
   }
 })
